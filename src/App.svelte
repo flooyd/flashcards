8e6b584e1aff427e9061311cfcc42fb1486c3f2a
Floyd Jones <floydtjones@gmail.com>
2021-11-11 18:50:47 -0600

    initial

---
+++ src/App.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+import Navbar from "./Navbar.svelte";
+</script>
+
+<main>
+	<Navbar/>
+  <div>There will be flashcards here soon.</div>
+  <div>Maybe even some decks full of flashcards...</div>
+</main>
+
+<style>
+  div {
+    margin: 10px 20px;
+  }
+</style>
---
+++ src/Navbar.svelte
@@ -0,0 +1,42 @@
+<script>
+
+</script>
+
+<nav>
+  <div class="title">Flashcards</div>
+  <div class="options">
+    <div>Login/Register</div>
+    <div>About</div>
+  </div>
+</nav>
+
+<style>
+  nav {
+    display: flex;
+    align-items: center;
+    font-size: 20px;
+    justify-content: space-between;
+    background: lightblue;
+    padding: 8px 20px;
+    border-bottom: 1px solid rgba(0,0,255,.5);
+  }
+
+  .title {
+    font-size: 25px;
+    font-weight: bold;
+  }
+
+  .options {
+    display: flex;
+    align-items: center;
+  }
+
+  .options div {
+    margin-left: 20px;
+  }
+
+  .options div:hover {
+    cursor: pointer;
+    color: white;
+  }
+</style>
